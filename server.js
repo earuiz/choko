fca344513074e25deb23e016669a6a4b09f99c59
Henrique Recidive <recidive@gmail.com>
2014-11-14 18:29:46 -0200

    Initial work on making Choko single application only. Issue #143.

--- server.js
+++ server.js
@@ -1,15 +1,76 @@
 /*
- * Create and initialize the Choko server.
+ * Create and initialize the Choko application.
  */
-var Server = require('./lib/server');
+
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+var lodash = require('lodash');
+var Application = require('./lib/application');
 
 var args = getArguments();
+var settings = getSettings(args.path);
 
-var server = new Server(args.path);
-server.start(args.port);
+var application = new Application(settings);
+application.start(args.port);
 
-// Get port and applications folder path from command line arguments. Use port
-// from "-p" or fallback to 3000. Default folder is ./applicaions.
+/**
+ * Build settings object.
+ */
+function getSettings(appDir) {
+  var modes = [];
+  var mergingSettings = [];
+  var applicationPaths = {
+    default: path.resolve('./applications/default'),
+    main: path.resolve(appDir)
+  };
+
+  // Always add environment mode.
+  modes.push(process.env.NODE_ENV);
+
+  // Default settings.
+  var settings = {
+    baseDir: path.resolve(__dirname),
+    applicationDir: path.resolve(appDir)
+  };
+
+  // Loop through applications.
+  Object.keys(applicationPaths).forEach(function(name) {
+
+    // Initiate possible settings files paths array.
+    var applicationSettings = [];
+
+    // Add default main settings file for this application.
+    applicationSettings.push(applicationPaths[name] + '/settings.json');
+    applicationSettings.push(applicationPaths[name] + '/settings.local.json');
+
+    // Loop through modes to register possible file paths.
+    modes.forEach(function(mode) {
+      var filePath = applicationPaths[name] + '/settings.' + mode;
+
+      // Register possible file paths for both 'mode' and 'mode.local'.
+      applicationSettings.push(filePath + '.json');
+      applicationSettings.push(filePath + '.local.json');
+    });
+
+    // Loop through defined file paths to load the ones that exist.
+    applicationSettings.forEach(function(filePath) {
+      if (fs.existsSync(filePath)) {
+        mergingSettings.push(JSON.parse(fs.readFileSync(path.relative(settings.baseDir, filePath), 'utf-8')));
+      }
+    });
+  });
+
+  // Load and merge all the settings.
+  lodash.merge.apply(null, [settings].concat(mergingSettings));
+
+  return settings;
+}
+
+/**
+ * Get port and applications folder path from command line arguments. Use port
+ * from "-p" or fallback to 3000. Default folder is ./applicaions.
+ */
 function getArguments() {
   var port = 3000;
   var path = './applications';
